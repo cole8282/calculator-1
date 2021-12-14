56a091d6a97bac9bdf4f388ac64c455b46892e9c
Cole Hendershot <colehendershot@gmail.com>
2021-12-14 15:39:27 -0700

    Completed Calculator Assignment

--- calc.js
+++ calc.js
@@ -9,11 +9,34 @@ const reader = readline.createInterface({
 
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
-	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
 
+	mathSymbol = tokens[0];
+    num1 = Number(tokens[1]);
+	  num2 = Number(tokens[2]);
+
+	  console.log("mathSymbol", mathSymbol);
+    console.log("num1", num1);
+    console.log("num2", num2);
+//addition
+	if (mathSymbol === '+') {
+		console.log(num1 + num2);
+	};
+//subtraction
+	if (mathSymbol === '-') {
+		console.log(num1 - num2);
+	}
+//multiplication
+	if (mathSymbol === '*') {
+		console.log(num1 * num2);
+	}
+//division
+  if (mathSymbol === '/') {
+		console.log(num1 / num2);
+	}
+//sqr
+  if (mathSymbol === 'sqrt') {
+		console.log(Math.sqrt(num1));
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
